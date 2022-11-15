738d836128224eeb004b9e4cd89cf3dbb664f80f
Daniel Mössner <38765196+danielmoessner@users.noreply.github.com>
2022-11-15 10:53:42 +0100

    improve login

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -1,28 +1,19 @@
-<template>
-  <div class="flex items-center justify-center w-full min-h-[80vh] px-5">
-    <div class="w-full max-w-md mx-auto">
-      <h1 class="text-3xl font-bold">Login</h1>
-      <div v-if="!userStore.isAuthenticated" class="mt-6">
-        <ButtonNormal @click="login()">To the login page</ButtonNormal>
-      </div>
-      <div v-else>
-        <ButtonNormal :to="{ name: 'statistics' }">
-          To the dashboard
-        </ButtonNormal>
-      </div>
-    </div>
-  </div>
-</template>
+<template><div class="p-2 text-sm">One moment please...</div></template>
 
 <script lang="ts" setup>
-import { watch } from "vue";
 import { useUserStore } from "@/store/user";
-import { ButtonNormal } from "@lawandorga/components";
 import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
 const router = useRouter();
 const userStore = useUserStore();
+
+// next
+const next = () => {
+  const url = route.query.next as string;
+  if (url) router.push(url);
+  else router.push({ name: "statistics" });
+};
 
 // login
 const login = () => {
@@ -32,14 +23,16 @@ const login = () => {
   window.location.href = url;
 };
 
-// if (!userStore.isAuthenticated) login();
-
-// redirect
-watch(userStore, () => {
-  if (userStore.isAuthenticated) {
-    const url = route.query.next as string;
-    if (url) router.push(url);
-    else router.push({ name: "statistics" });
-  }
-});
+// login or go to statistics
+if (userStore.isAuthenticated) next();
+else {
+  userStore
+    .updateData()
+    .then(() => {
+      next();
+    })
+    .catch(() => {
+      login();
+    });
+}
 </script>
