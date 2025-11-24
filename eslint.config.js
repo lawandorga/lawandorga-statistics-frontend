d185297e249d892ffb97800414d40730ccc92f51
JoeyStk <johannes.stock@yahoo.de>
2025-11-24 15:42:35 +0100

    Add eslint

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import globals from "globals";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import path from "path";
+import { fileURLToPath } from "url";
+import typescriptParser from "@typescript-eslint/parser";
+import vueParser from "vue-eslint-parser";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const eslintrc = new FlatCompat({
+  baseDirectory: __dirname,
+  resolvePluginsRelativeTo: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
+      parser: vueParser,
+      parserOptions: {
+        parser: typescriptParser,
+      },
+    },
+  },
+  ...eslintrc.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:vue/recommended",
+  ),
+  ...eslintrc.config({
+    rules: {
+      "no-unused-vars": "warn",
+      "vue/no-unused-components": "warn",
+      "vue/require-default-prop": "off",
+      "vue/component-name-in-template-casing": [
+        "error",
+        "PascalCase",
+        {
+          ignores: [],
+        },
+      ],
+    },
+  }),
+];
