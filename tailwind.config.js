93f1cb6e5384a7a3ab6463ea4d181ca13d5b55b0
JoeyStk <johannes.stock@yahoo.de>
2025-11-25 12:32:48 +0100

    Update vite.config and remove tailwind.config

--- tailwind.config.js
+++
@@ -1,5 +0,0 @@
-import lawandorga from 'lorga-ui';
-
-const content = ["index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"];
-
-module.exports = lawandorga.utils.getTailwindConfig(content);
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 import dns from "dns";
+import tailwindcss from "@tailwindcss/vite";
 
 // sets localhost as default instead of 127.0.0.1
 dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   server: {
     port: 4300,
     host: "localhost",
