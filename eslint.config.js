a52cdcea7238d0ce0d146c03fc2fcfa291f026ff
JoeyStk <johannes.stock@yahoo.de>
2025-11-25 11:46:24 +0100

    Fix pipeline

--- eslint.config.js
+++ eslint.config.js
@@ -1,61 +1,28 @@
 import globals from "globals";
-import { FlatCompat } from "@eslint/eslintrc";
 import js from "@eslint/js";
-import path from "path";
-import { fileURLToPath } from "url";
-import typescriptParser from "@typescript-eslint/parser";
 import vueParser from "vue-eslint-parser";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const eslintrc = new FlatCompat({
-  baseDirectory: __dirname,
-  resolvePluginsRelativeTo: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import tsParser from "@typescript-eslint/parser";
 
 export default [
+  js.configs.recommended,
+
   {
+    files: ["**/*.ts", "**/*.vue"],
+
     languageOptions: {
-      ecmaVersion: 2023,
-      sourceType: "module",
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 2023,
+        sourceType: "module",
+      },
       globals: {
         ...globals.browser,
       },
-      parser: vueParser,
-      parserOptions: {
-        parser: typescriptParser,
-      },
     },
-  },
-  ...eslintrc.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:vue/recommended",
-  ),
-  ...eslintrc.config({
+
     rules: {
       "no-unused-vars": "warn",
-      "vue/no-unused-components": "warn",
-      "vue/require-default-prop": "off",
-      "vue/component-name-in-template-casing": [
-        "error",
-        "PascalCase",
-        {
-          ignores: [],
-        },
-      ],
-      "@typescript-eslint/ban-types": [
-        "error",
-        {
-          types: {
-            "{}": false,
-          },
-          extendDefaults: true,
-        },
-      ],
     },
-  }),
+  },
 ];
--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -2,6 +2,7 @@ import { Router } from "vue-router";
 
 declare module "@vue/runtime-core" {
    
+  /* eslint-disable-next-line */
   interface ComponentCustomProperties {
     $router: Router;
   }
